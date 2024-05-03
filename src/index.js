237547b8a6266c0ac5ba4bc283e02d2e41c5ace9
gabiaguilerav <gabiaguilerava@gmail.com>
2024-05-03 11:56:21 +0200

    JS added: the form now works

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
+function generateRecipe(event) {
+  event.preventDefault();
 
+  new Typewriter("#recipe-text", {
+    strings: "You must cook your vegetables...",
+    autoStart: true,
+    cursor: null,
+    delay: 20,
+  });
+}
+let recipeGenerator = document.querySelector("#generator-form");
+recipeGenerator.addEventListener("submit", generateRecipe);
