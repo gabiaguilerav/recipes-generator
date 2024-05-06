49636e8c5aabdf5ee5a8023b66543a85d7baa042
gabiaguilerav <gabiaguilerava@gmail.com>
2024-05-06 10:46:29 +0200

    AI API integration

--- src/index.js
+++ src/index.js
@@ -1,12 +1,30 @@
+function displayRecipe(response) {
+  console.log(response.data.answer);
+
+  new Typewriter("#recipe-text", {
+    strings: response.data.answer,
+    autoStart: true,
+    cursor: null,
+    delay: 10,
+  });
+}
+
 function generateRecipe(event) {
   event.preventDefault();
 
-  new Typewriter("#recipe-text", {
-    strings: "You must cook your vegetables...",
-    autoStart: true,
-    cursor: null,
-    delay: 20,
-  });
+  let instructionsInput = document.querySelector("#user-instructions");
+  let apiKey = "7145tacf48aad88be65c0b31aoec2f85";
+  let context =
+    "You are an AI that knows a lot about vegan food, be kind and give easy answers with tasty and quick vegan recipes. Sign the recipe with 'We hope you enjoy your meal -SheCodes AI', inside a <small> element at the end and";
+  let prompt = `Tell me a quick and easy vegan recipe based on the ingredients ${instructionsInput.value} that I have right now available at home`;
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  console.log("Generating recipe");
+  console.log(`Prompt:${prompt}`);
+  console.log(`context:${context}`);
+
+  axios.get(apiUrl).then(displayRecipe);
 }
+
 let recipeGenerator = document.querySelector("#generator-form");
 recipeGenerator.addEventListener("submit", generateRecipe);
