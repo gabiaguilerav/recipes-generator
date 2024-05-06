29b95348a7b65cb87da28d60ef44ae51bf0a458d
gabiaguilerav <gabiaguilerava@gmail.com>
2024-05-06 12:18:19 +0200

    UX improved

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ function displayRecipe(response) {
     strings: response.data.answer,
     autoStart: true,
     cursor: null,
-    delay: 10,
+    delay: 6,
   });
 }
 
@@ -15,9 +15,12 @@ function generateRecipe(event) {
   let instructionsInput = document.querySelector("#user-instructions");
   let apiKey = "7145tacf48aad88be65c0b31aoec2f85";
   let context =
-    "You are an AI that knows a lot about vegan food, be kind and give easy answers with tasty and quick vegan recipes. Sign the recipe with 'We hope you enjoy your meal -SheCodes AI', inside a <small> element at the end and";
+    "You are an AI that knows a lot about vegan food, be kind and give easy answers with tasty and quick vegan recipes. Separate every new sentence or step with a <br> element. Sign the recipe with '-SheCodes AI' at the end";
   let prompt = `Tell me a quick and easy vegan recipe based on the ingredients ${instructionsInput.value} that I have right now available at home`;
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  let recipeElement = document.querySelector("#recipe");
+  recipeElement.classList.remove("recipe-hidden");
 
   console.log("Generating recipe");
   console.log(`Prompt:${prompt}`);
